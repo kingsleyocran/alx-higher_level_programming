bdd77c186a7bfd09cd964722603919a7ebe02318
kingsleyocran <ocrankingsley@gmail.com>
2022-06-10 00:25:22 +0000

    modified task 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,5 +1,16 @@
 #!/usr/bin/python3
-def best_score(a_dictionary):
-    if not a_dictionary:
+def best_score(my_dict):
+    if my_dict is None or my_dict == {}:
         return None
-    return max(a_dictionary)
+    biggest = max(my_dict.values())
+    for key, value in my_dict.items():
+        if value is biggest:
+            return key
+
+
+a_dictionary = {'John': 12, 'Bob': 14, 'Mike': 14, 'Molly': 16, 'Adam': 10}
+best_key = best_score(a_dictionary)
+print("Best score: {}".format(best_key))
+
+best_key = best_score(None)
+print("Best score: {}".format(best_key))
