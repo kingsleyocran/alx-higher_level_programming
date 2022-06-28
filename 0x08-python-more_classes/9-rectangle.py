0ce18a4bf1596444e02fadc9296ac535d40458d0
kingsleyocran <ocrankingsley@gmail.com>
2022-06-28 00:08:21 +0000

    add task 8 and 9

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -141,8 +141,3 @@ class Rectangle:
             The new Rectangle instance
         """
         return cls(size, size)
-
-
-my_square = Rectangle.square(5)
-print("Area: {} - Perimeter: {}".format(my_square.area(), my_square.perimeter()))
-print(my_square)
