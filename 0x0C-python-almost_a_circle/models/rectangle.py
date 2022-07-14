3bcbe9bd2f3de99aee4d57a388b9f183fee24b7f
kingsleyocran <ocrankingsley@gmail.com>
2022-07-14 22:00:29 +0000

    all task

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -8,17 +8,17 @@ from models.base import Base
 
 class Rectangle(Base):
     """Class describing a rectangle.
-    Public instance methods:
+    - Public instance methods:
         - area()
         - display()
         - to_dictionary()
         - update()
-    Inherits from Base.
+    - Inherits from Base.
     """
 
     def __init__(self, width, height, x=0, y=0, id=None):
         """Initializes a Rectangle instance.
-        Args:
+        - Args:
             - __width: width
             - __height: height
             - __x: position
@@ -32,30 +32,6 @@ class Rectangle(Base):
         self.y = y
         super().__init__(id)
 
-    @property
-    def width(self):
-        """Retrieves the width attribute."""
-
-        return self.__width
-
-    @property
-    def height(self):
-        """Retrieves the height attribute."""
-
-        return self.__height
-
-    @property
-    def x(self):
-        """Retrieves the x attribute."""
-
-        return self.__x
-
-    @property
-    def y(self):
-        """Retrieves the y attribute."""
-
-        return self.__y
-
     @width.setter
     def width(self, value):
         """Sets the width attribute."""
@@ -66,9 +42,19 @@ class Rectangle(Base):
             raise ValueError("width must be > 0")
         self.__width = value
 
+    @property
+    def width(self):
+        """Getter method for width
+        Retrieves the width attribute.
+        """
+
+        return self.__width
+
     @height.setter
     def height(self, value):
-        """Sets the height attribute."""
+        """ Setter method for height
+        - Sets the height attribute.
+        """
 
         if type(value) is not int:
             raise TypeError("height must be an integer")
@@ -76,9 +62,19 @@ class Rectangle(Base):
             raise ValueError("height must be > 0")
         self.__height = value
 
+    @property
+    def height(self):
+        """ Getter method for height
+        - Retrieves the height attribute.
+        """
+
+        return self.__height
+
     @x.setter
     def x(self, value):
-        """Sets the x attribute."""
+        """Setter method for x
+        - Sets the x attribute.
+        """
 
         if type(value) is not int:
             raise TypeError("x must be an integer")
@@ -86,15 +82,33 @@ class Rectangle(Base):
             raise ValueError("x must be >= 0")
         self.__x = value
 
+    @property
+    def x(self):
+        """Getter method for x
+        - Retrieves the x attribute.
+        """
+
+        return self.__x
+
     @y.setter
     def y(self, value):
-        """Sets the y attribute."""
+        """Setter method for y
+        - Sets the y attribute.
+        """
 
         if type(value) is not int:
             raise TypeError("y must be an integer")
         if value < 0:
             raise ValueError("y must be >= 0")
         self.__y = value
+
+    @property
+    def y(self):
+        """Getter method for y
+        - Retrieves the y attribute.
+        """
+
+        return self.__y
 
     def area(self):
         """Calculates the area of a Rectangle instance.
@@ -124,7 +138,7 @@ class Rectangle(Base):
 
     def update(self, *args, **kwargs):
         """Updates attributes of an instance.
-        Args:
+        - Args:
             - id attribute
             - width attribute
             - height attribute
