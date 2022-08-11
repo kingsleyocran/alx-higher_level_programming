d7895e6c83e32b16b15bac49b11b2406f1a47a73
kingsleyocran <ocrankingsley@gmail.com>
2022-08-11 20:50:30 +0000

    fix task 7

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -2,7 +2,7 @@
 -- cities description:
 -- - id INT unique, auto generated, can’t be null and is a primary key
 -- - state_id INT, can’t be null and must be a FOREIGN KEY that references to id of the states table
-name VARCHAR(256) can’t be null
+-- - name VARCHAR(256) can’t be null
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 -- use a database
 USE hbtn_0d_usa;
