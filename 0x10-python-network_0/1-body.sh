5aaae9197f955f3e865f022c2b415b2806cbba8b
kingsleyocran <ocrankingsley@gmail.com>
2022-09-25 21:51:14 +0000

    add fixes

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-# takes in a URL, sends a GET request to the URL, and displays the body of the response
+# takes in a URL, sends a GET request to the URL,
+# and displays the body of a 200 status code response
 curl -s "$1" -X GET -L 
