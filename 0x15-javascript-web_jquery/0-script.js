c59c061d52e315279c163f7f9f59321192a00884
kingsleyocran <ocrankingsley@gmail.com>
2022-10-27 00:36:10 +0000

    add files

---
+++ 0x15-javascript-web_jquery/0-script.js
@@ -0,0 +1,4 @@
+// Javascript script that updates the text color of the <header> element to red (#FF0000)
+
+const colorHeader = document.querySelector('header');
+colorHeader.style.color = '#FF0000';
